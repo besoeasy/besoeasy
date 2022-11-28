87853a9017f041e9fea042836caeb019e5a44c94
Aman Singh Katal <besoeasy@users.noreply.github.com>
2022-11-28 06:17:51 +0530

    more socials

--- src/components/social.vue
+++ src/components/social.vue
@@ -4,4 +4,10 @@
 		<a href="https://www.linkedin.com/in/besoeasy">✅ linkedin</a>
 		<a href="https://t.me/besoeasy">✅ Telegram</a>
 	</div>
+
+	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
+		<a href="https://instagram.com/besoeasy">✅ Instagram</a>
+		<a href="https://steamcommunity.com/id/besoeasy">✅ Steam</a>
+		<a href="https://last.fm/user/besoeasy">✅ Last.fm</a>
+	</div>
 </template>

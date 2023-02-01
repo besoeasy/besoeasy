251511e344057544988024a59d7e207c7695a112
Aman Singh Katal <besoeasy@users.noreply.github.com>
2023-02-02 04:25:30 +0530

    Update social.vue

--- src/components/social.vue
+++ src/components/social.vue
@@ -10,6 +10,6 @@
 	</div>
 
 	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
-		<a href="https://t.me/besoeasy">Talk To Me 👉 Telegram</a>
+		<a href="https://t.me/besoeasy">🚀 Telegram</a>
 	</div>
 </template>

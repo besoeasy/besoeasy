128d5adfb954f8bf0e229a583d4c6fc65a643796
Aman Singh Katal <besoeasy@users.noreply.github.com>
2023-02-02 04:21:53 +0530

    Update social.vue

--- src/components/social.vue
+++ src/components/social.vue
@@ -2,11 +2,14 @@
 	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
 		<a href="https://github.com/besoeasy">✅ Github</a>
 		<a href="https://www.linkedin.com/in/besoeasy">✅ linkedin</a>
-		<a href="https://t.me/besoeasy">✅ Telegram</a>
 	</div>
 
 	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
 		<a href="https://instagram.com/besoeasy">✅ Instagram</a>
 		<a href="https://steamcommunity.com/id/besoeasy">✅ Steam</a>
-\	</div>
+	</div>
+
+	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
+		<a href="https://t.me/besoeasy">Talk To Me 👉 Telegram</a>
+	</div>
 </template>

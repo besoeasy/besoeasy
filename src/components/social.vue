1c0ebb5335e745dc22dad61f4806cc4c3255e63e
Aman Singh Katal <besoeasy@users.noreply.github.com>
2023-04-22 01:04:19 +0530

    Update social.vue

--- src/components/social.vue
+++ src/components/social.vue
@@ -1,15 +1,9 @@
 <template>
-	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
-		<a href="https://github.com/besoeasy">✅ Github</a>
-		<a href="https://www.linkedin.com/in/besoeasy">✅ linkedin</a>
-	</div>
-
-	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
-		<a href="https://instagram.com/besoeasy">✅ Instagram</a>
-		<a href="https://steamcommunity.com/id/besoeasy">✅ Steam</a>
-	</div>
-
-	<div class="w-full flex sm:flex-row flex-col font-medium sm:space-x-5 space-y-4 sm:space-y-0 uppercase text-red-500">
-		<a href="https://t.me/besoeasy">🚀 Telegram</a>
-	</div>
+   <div class="grid grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-4 font-medium ">
+      <a href="https://instagram.com/besoeasy">✅ Instagram</a>
+      <a href="https://steamcommunity.com/id/besoeasy">✅ Steam</a>	
+      <a href="https://github.com/besoeasy">✅ Github</a>
+      <a href="https://www.linkedin.com/in/besoeasy">✅ linkedin</a>
+      <a href="https://t.me/besoeasy">🚀 Telegram</a>
+   </div>
 </template>

eee8d48bad2819c063bc8125725ab2d95f370336
Aman Singh Katal <besoeasy@users.noreply.github.com>
2023-05-12 21:11:02 +0530

    Update social.vue

--- src/components/social.vue
+++ src/components/social.vue
@@ -3,7 +3,8 @@
       <a href="https://instagram.com/besoeasy">✅ Instagram</a>
       <a href="https://steamcommunity.com/id/besoeasy">✅ Steam</a>	
       <a href="https://github.com/besoeasy">✅ Github</a>
-      <a href="https://www.linkedin.com/in/besoeasy">✅ linkedin</a>
+      <a href="https://www.linkedin.com/in/besoeasy">✅ Linkedin</a>
+      <a href="https://besoeasy.medium.com">✅ Medium</a>
       <a href="https://t.me/besoeasy">🚀 Telegram</a>
    </div>
 </template>

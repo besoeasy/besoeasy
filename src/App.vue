7b3d3070cb481c0932c4be37482f7859ecb63c65
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-02-29 02:51:41 +0530

    quick margins

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
 	<div class="p-5 m-auto max-w-7xl">
 		<div class="flex sm:flex-row flex-col gap-10 items-center justify-center min-h-screen">
-			<div class="w-full md:w-1/2">
+			<div class="w-full md:w-3/5">
 				<div class="m-auto transform hover:-translate-y-1 transition duration-500">
 					<img class="rounded-2xl" src="/me.png" alt="" />
 				</div>
 			</div>
-			<div class="w-full md:w-1/2">
+			<div class="w-full md:w-2/5">
 				<div class="m-auto">
 					<div class="text-2xl lg:text-4xl mb-2 text-pretty">AMAN SINGH KATAL</div>
 					<div class="text-xl mb-5 font-semibold">

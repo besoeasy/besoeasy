ad8885bc9923a9d9383bf923ed5831b63542a113
besoeasy <besoeasy@users.noreply.github.com>
2025-12-25 06:41:05 +0530

    refactor: improve formatting and readability in App.vue; remove unused externalLinks object

--- src/App.vue
+++ src/App.vue
@@ -8,33 +8,37 @@
       </div>
       <div class="w-full md:w-2/5">
         <div class="m-auto">
-          <div class="text-2xl lg:text-5xl mb-2 text-pretty font-extrabold animate-pulse hover:animate-bounce hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-orange-500 hover:via-red-500 hover:to-pink-500 transition-all duration-500 cursor-pointer">AMAN SINGH KATAL</div>
+          <div
+            class="text-2xl lg:text-5xl mb-2 text-pretty font-extrabold animate-pulse hover:animate-bounce hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-orange-500 hover:via-red-500 hover:to-pink-500 transition-all duration-500 cursor-pointer"
+          >
+            AMAN SINGH KATAL
+          </div>
           <div class="text-xl mb-5 font-semibold animate-pulse">
             <ContactEmail />
           </div>
 
           <div class="mb-5 text-lg text-gray-800">
-            I am a seasoned full-stack developer 
+            I am a seasoned full-stack developer
             <span class="inline-flex items-center mx-1 cursor-pointer transform hover:scale-125 hover:rotate-12 transition-all duration-300">
               <Code2 :size="20" class="text-blue-600" />
             </span>
-            with a primary focus on fintech 
+            with a primary focus on fintech
             <span class="inline-flex items-center mx-1 cursor-pointer transform hover:scale-125 hover:rotate-12 transition-all duration-300">
               <DollarSign :size="20" class="text-green-600" />
             </span>
-            and automation 
+            and automation
             <span class="inline-flex items-center mx-1 cursor-pointer transform hover:scale-125 hover:rotate-12 transition-all duration-300">
               <Zap :size="20" class="text-yellow-600" />
             </span>
-            . Proficient in a wide range of programming languages 
+            . Proficient in a wide range of programming languages
             <span class="inline-flex items-center mx-1 cursor-pointer transform hover:scale-125 hover:rotate-12 transition-all duration-300">
               <Layers :size="20" class="text-purple-600" />
             </span>
-            , I have collaborated with 70+ organizations 
+            , I have collaborated with 70+ organizations
             <span class="inline-flex items-center mx-1 cursor-pointer transform hover:scale-125 hover:rotate-12 transition-all duration-300">
               <Users :size="20" class="text-orange-600" />
             </span>
-            , delivering mission-critical applications handling substantial financial and sensitive data 
+            , delivering mission-critical applications handling substantial financial and sensitive data
             <span class="inline-flex items-center mx-1 cursor-pointer transform hover:scale-125 hover:rotate-12 transition-all duration-300">
               <ShieldCheck :size="20" class="text-red-600" />
             </span>
@@ -43,11 +47,6 @@
 
           <div class="space-y-3 leading-6 text-left xl:space-y-5 uppercase">
             <div v-for="([name, url], idx) in socialLinks" :key="name" class="relative inline-block mr-3">
-              <Sbut :textinput="name" :SocialLinks="url" />
-            </div>
-          </div>
-          <div class="space-y-3 leading-6 text-left xl:space-y-5 uppercase">
-            <div v-for="([name, url], idx) in externalLinks" :key="name" class="relative inline-block mr-3">
               <Sbut :textinput="name" :SocialLinks="url" />
             </div>
           </div>
@@ -66,10 +65,8 @@ import { Code2, DollarSign, Zap, Layers, Users, ShieldCheck } from "lucide-vue-n
 const socialLinksObj = {
   Github: "https://github.com/besoeasy",
   NOSTR: "https://nosta.me/besoeasy@zaps.lol",
-};
-
-const extrainksObj = {
   Blog: "https://tinyurl.com/besoeasyblog",
+  steam: "https://steamcommunity.com/id/besoeasy",
 };
 
 function shuffle(array) {
@@ -81,5 +78,4 @@ function shuffle(array) {
 }
 
 const socialLinks = ref(shuffle(Object.entries(socialLinksObj)));
-const externalLinks = ref(shuffle(Object.entries(extrainksObj)));
 </script>
